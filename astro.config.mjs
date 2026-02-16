87911bf4cf74716878076e805a6c06ade598972f
rayhan-wijaya <rayhanwijaya210@gmail.com>
2026-02-16 23:28:04 +0700

    defined env schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import vercelAdapter from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+	env: {
+		schema: {
+			MC_HOST: envField.string({
+				context: "server",
+				access: "secret",
+			}),
+			MC_PORT: envField.number({
+				context: "server",
+				access: "secret",
+				default: 25565,
+			}),
+			MC_PROTOCOL_VERSION: envField.number({
+				context: "server",
+				access: "secret",
+			}),
+		},
+	},
 	adapter: vercelAdapter({
 		imageService: true,
 		webAnalytics: {
